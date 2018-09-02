1764146bfc9c98200b795edf2f37331057709c8a
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-09-02 13:49:45 -0500

    first commit

---
+++ string4_22.js
@@ -0,0 +1,9 @@
+//Write a function which takes in a string and returns counts of each character in the string.
+
+//make an object to return at the end
+//foreach letter/number
+    //store value of a letter/number
+    //check if letter/number exist in the object key
+        //if it does exsist add 1 to the object key value 
+    //else create a new object key and set to 1
+
