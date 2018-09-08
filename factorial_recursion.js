710c9c7529095ef0939d529f0b54b1602f46957e
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-09-08 13:27:41 -0500

    factorial problem created

---
+++ factorial_recursion.js
@@ -0,0 +1,11 @@
+//write a factorial using recursion
+
+   //base case: num equal zero return 1
+   //take in an input 
+   //subtract 1 from input until 1 is reach
+   //repeat
+   //return to previous numbers and multiple
+
+function factorial(){
+ 
+}
