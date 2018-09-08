dfcf2fb1cee96252028c15f82efb767a9b00ce11
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-09-08 09:48:28 -0500

    helper method create

--- helper_recursion.js
+++ helper_recursion.js
@@ -8,4 +8,23 @@
 //remove element from the array
 //repeat 
 //base case: array is empty
+//return the array of odd numbers
 
+function collectOddValues(arr){
+
+    let oddArr = [];
+     function helper(arr) {
+         if(arr.length === 0){
+             return
+         }
+         if(arr[0] % 2 !== 0){
+            oddArr.push(arr[0])
+         }
+         helper(arr.slice(1));
+     }
+     helper(arr);
+
+     return oddArr;
+}
+ let arr2 = [9,3,2,5,2,0,7,19];
+collectOddValues(arr2)
