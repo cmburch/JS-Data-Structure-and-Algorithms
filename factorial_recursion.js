f17e3cb9b63fbbd61943e6fa6ab8af1377853db7
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-09-08 13:29:45 -0500

    factorial problem solved

--- factorial_recursion.js
+++ factorial_recursion.js
@@ -6,6 +6,18 @@
    //repeat
    //return to previous numbers and multiple
 
-function factorial(){
+   function factorial(num){
+    if(num === 0){
+        return 1;
+    }
+    
+    return num *factorial(num-1)
+ }
  
-}
+
+
+
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
