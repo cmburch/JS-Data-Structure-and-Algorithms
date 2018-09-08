e67e7d2ef9b36304f004b6c831846d2e01b819e2
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-09-08 13:16:35 -0500

    power of number solved

--- power_recursion.js
+++ power_recursion.js
@@ -1,5 +1,7 @@
 //write a funtions that calculate the power of a number mimic Math.pow();
 
-function power(base,exponent){
 
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base,exponent-1);
 }
