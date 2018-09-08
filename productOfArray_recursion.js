c7972c71a644e28e07950e9ea5a064b1a33596b7
Charles Burch <charlesburch@CBMacBook-Pro.local>
2018-09-08 13:42:42 -0500

    pseudo for productOfArray

--- productOfArray_recursion.js
+++ productOfArray_recursion.js
@@ -1,6 +1,18 @@
 //Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
 //use recursion
 
+    //base case if array is empty stop return 1
+    //take in input of array of numbers 
+    //take the first element countdown by array length
+    //repeat
+    //return each number to previous line and multiple
+    
 function productOfArray(){
     
 }
+
+
+
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
